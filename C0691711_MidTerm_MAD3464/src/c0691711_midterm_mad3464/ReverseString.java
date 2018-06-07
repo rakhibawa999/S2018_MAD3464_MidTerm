3a0ccf87ba8c014dcb8a379f53709221f34f314c
macstudent <macstudent@10.10.150.15>
2018-06-07 15:36:50 -0400

    reverse

---
+++ C0691711_MidTerm_MAD3464/src/c0691711_midterm_mad3464/ReverseString.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0691711_midterm_mad3464;
+
+
+/**
+ *
+ * @author macstudent
+ */
+public class ReverseString {
+    String input ="Lambton";
+    char[] temp = input.toCharArray();
+    int left =0;
+    int right =0;
+    right= temp.length -1;
+    
+        
+    
+    for(left=0 ; left<right ; left++ ; right--)
+    {
+        char t =temp [left];
+        temp[left]=temp[right];
+        temp[right]=t;
+        
+    }    
+     for (char c :t)
+     {
+     System.out.println(c);
+     System.out.println();
+     }
+
+    
+    }
+
+    
