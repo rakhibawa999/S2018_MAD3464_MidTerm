571ab183400d0a87a01de8e6a19f3ae1cee05083
macstudent <macstudent@10.10.150.15>
2018-06-07 16:52:18 -0400

    lANTON

---
+++ C0691711_MidTerm_MAD3464/src/c0691711_midterm_mad3464/Lambton.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0691711_midterm_mad3464;
+
+/**
+ *
+ * @author macstudent
+ */public class Lambton {
+    public Lambton() {
+}
+
+public String reverse(String s) {
+    char r[] = s.toCharArray();
+
+    for (int i = r.length - 1; i >= 0; i--) {
+        System.out.print(r[i]);
+
+    }
+    return null;
+}
+    
+}
